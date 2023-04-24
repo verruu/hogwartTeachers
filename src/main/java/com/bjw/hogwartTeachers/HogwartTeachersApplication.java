3b31ccd38cb9aa53383351d3c6cd8362c52d3e08
verruu <gacek333@gmail.com>
2023-04-25 00:23:33 +0200

    Innitial commit.

---
+++ src/main/java/com/bjw/hogwartTeachers/HogwartTeachersApplication.java
@@ -0,0 +1,13 @@
+package com.bjw.hogwartTeachers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartTeachersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartTeachersApplication.class, args);
+    }
+
+}
